1a1d36c26fe058273e3a09f15a507c3fe1443578
piotrszwarnog <piotrszwarnog@gmail.com>
2016-06-05 21:49:26 +0000

    neat cosmetic changes

--- app/index.js
+++ app/index.js
@@ -4,10 +4,6 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import routing from './config';
 import tuduapp from './features/tuduapp';
-
-  // var app=angular.module('app', [uirouter, angularFire]);
-  // app.controller('tuduap', TuduappController)
-  // .name;
   
    angular
    .module('app', [uirouter, tuduapp])
